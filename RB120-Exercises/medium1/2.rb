ea154a6ccadea8771667c586747d27453bbf4ca3
Ainaa Azman <ainaasakinah@gmail.com>
2021-02-23 16:38:51 +0900

    add: exercise 2

---
+++ RB120-Exercises/medium1/2.rb
@@ -0,0 +1,71 @@
+# Write a FixedArray class that always takes a fixed-length array
+# and the necessary method that support the code below.
+
+class FixedArray
+  def initialize(number)
+    @array = [nil] * number
+  end
+
+  def [](index)
+    @array.fetch(index)
+  end
+
+  def []=(index, input)
+    if self[index] || self[index].nil?
+      @array[index] = input
+    end
+  end
+
+  def to_a
+    @array.clone
+  end
+
+  def to_s
+    to_a.to_s
+  end
+end
+
+fixed_array = FixedArray.new(5)
+puts fixed_array[3] == nil
+puts fixed_array.to_a == [nil] * 5
+
+fixed_array[3] = 'a'
+puts fixed_array[3] == 'a'
+puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
+
+fixed_array[1] = 'b'
+puts fixed_array[1] == 'b'
+puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
+
+fixed_array[1] = 'c'
+puts fixed_array[1] == 'c'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
+
+fixed_array[4] = 'd'
+puts fixed_array[4] == 'd'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
+puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
+
+puts fixed_array[-1] == 'd'
+puts fixed_array[-4] == 'c'
+
+begin
+  fixed_array[6]
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[-7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
