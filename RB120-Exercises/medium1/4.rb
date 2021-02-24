cbb67770a7499577439495527e39eb0fe2a64901
Ainaa Azman <ainaasakinah@gmail.com>
2021-02-24 17:10:43 +0900

    add: circular queue soluetions

--- RB120-Exercises/medium1/4.rb
+++ RB120-Exercises/medium1/4.rb
@@ -1,51 +1,89 @@
+
+
 class CircularQueue
   def initialize(size)
-    @array = Array.new(size)
+    @buffer = Array.new(size)
     @next_position = 0
     @oldest_position = 0
   end
 
-  def enqueue(number)
+  def enqueue(object)
+    dequeue if full?
+    buffer[next_position] = object
     find_next_position
-    find_oldest_position
-    if full?
-      @array[@oldest_position] = number
-    else
-      @array[@next_position] = number
-    end
-  end
-
-  def find_next_position
-    @array.each_with_index do |element, index|
-      return @next_position = index if element.nil?
-    end
-    @next_position = nil
-  end
-
-  def find_oldest_position
-    if empty?
-      @oldest_position = 0
-    else
-      numbers = @array.select { |element| element.is_a? Integer}
-      @oldest_position = @array.index(numbers.min)
-    end
   end
 
   def dequeue
-    find_oldest_position
-    removed = @array[@oldest_position]
-    @array[@oldest_position] = nil
+    removed = buffer[oldest_position]
+    buffer[oldest_position] = nil
+    self.oldest_position = (oldest_position + 1) % buffer.length unless removed.nil?
     removed
   end
 
-  def empty?
-    @array.all? { |element| element == nil }
+  private
+
+  attr_accessor :buffer, :next_position, :oldest_position
+
+  def find_next_position
+    self.next_position = (next_position + 1) % buffer.length
   end
 
   def full?
-    @array.all? { |element| element != nil }
+    buffer.count(nil) == 0
   end
 end
+
+# Attempt no 2 - Not quite understanding the circular queue
+
+# class CircularQueue
+#   def initialize(size)
+#     @array = Array.new(size)
+#     @next_position = 0
+#     @oldest_position = 0
+#   end
+
+#   def enqueue(number)
+#     find_next_position
+#     if full?
+#       dequeue
+#       @array[@oldest_position] = number
+#     else
+#       @array[@next_position] = number
+#     end
+#   end
+
+#   def find_next_position
+#     @array.each_with_index do |element, index|
+#       return @next_position = index if element.nil?
+#     end
+#   end
+
+#   def find_oldest_position
+#     if empty?
+#       @oldest_position = rand(@array.size)
+#     else
+#       numbers = @array.select { |element| element.is_a? Integer}
+#       @oldest_position = @array.index(numbers.min)
+#     end
+#   end
+
+#   def dequeue
+#     find_oldest_position
+#     removed = @array[@oldest_position]
+#     @array[@oldest_position] = nil
+#     removed
+#   end
+
+#   def empty?
+#     @array.all? { |element| element == nil }
+#   end
+
+#   def full?
+#     @array.all? { |element| element != nil }
+#   end
+# end
+
+# Attempt no 1 - Further Exploration answer
 
 # class CircularQueue
 #   def initialize(number)
@@ -78,14 +116,11 @@ queue.enqueue(3)
 queue.enqueue(4)
 puts queue.dequeue == 2
 
-queue.enqueue(5)
-p queue
-queue.enqueue(6)
-p queue
+queue.enqueue("a")
+queue.enqueue("b")
 queue.enqueue(7)
-p queue
-puts queue.dequeue == 5
-puts queue.dequeue == 6
+puts queue.dequeue == "a"
+puts queue.dequeue == "b"
 puts queue.dequeue == 7
 puts queue.dequeue == nil
 
