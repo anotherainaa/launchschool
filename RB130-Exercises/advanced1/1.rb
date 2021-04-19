1b55501aac095631cb674be05c1c217f854a2097
Ainaa Azman <ainaasakinah@gmail.com>
2021-04-19 20:10:11 +0900

    reviewing enumerators

--- RB130-Exercises/advanced1/1.rb
+++ RB130-Exercises/advanced1/1.rb
@@ -1,22 +1,7 @@
 # Internal vs External Iterators
 
-# Demo of Enummerator
-
-# class FakeEnum
-
-# end
-
-# def factorial(n)
-#   return 1 if n.zero?
-#   1.upto(n).inject do |sum, num|
-#     sum * num
-#   end
-# end
-
-
 # Hint - you'll only need 3 enum methods
 # You might need enumerable.
-
 
 # p factorial(0)
 # p factorial(1)
@@ -32,7 +17,7 @@
 # 4 24  4! = 4 * 3 * 2 * 1
 # 5 120 5! = 5 * 4 * 3 * 2 * 1
 
-factorial = Enumerator.new { |yielder|
+factorial = Enumerator.new do |yielder|
   accumulator = 1
   number = 0
   loop do
@@ -40,14 +25,66 @@ factorial = Enumerator.new { |yielder|
     yielder << accumulator
     number += 1
   end
-}
-
-# External iterator
-
-
-
-
-# Internal iterator
-factorial.each do |num|
-  factorial
 end
+
+# external iterators
+
+# 6.times { |number| puts "#{number}! == #{factorial.next}" }
+# puts "=========================="
+# 6.times { |number| puts "#{number}! == #{factorial.next}" }
+# puts "=========================="
+# factorial.rewind
+# 6.times { |number| puts "#{number}! == #{factorial.next}" }
+# puts "=========================="
+
+# When we don't rewind, the enumerator just keeps going from the last
+# place we stopped from the first time around?
+
+#internal iterators
+
+# factorial.each_with_index do |value, number|
+#   puts "#{number}! == #{value}"
+#   break if number >= 5
+# end
+
+# Here we have to break out of the loop because the original enumerator
+# doesn't have a breaking point.
+
+# Enumerators are the reason we can chain .map.with_index ?
+
+# .map - when called without a block returns an enumerator.
+# with the enumerator we can do stuff with it like check what is next
+# in the array.
+
+# we can chain methods
+# we can use internal iterators or external iterators
+
+# if we pass a block into it, we're just using it like we normally do it seems.
+
+# Enumerate means to mention a number one by one.
+# To specify one after the other.
+
+# In the factorial, we have a loop that should go on and on
+# because there is no breaking condition.
+# But with the enumerator we allow ourselves the ability to go over it
+# one at a time as however we want to.
+
+
+forever = Enumerator.new do |yielder|
+  number = 0
+  loop do
+    yielder << number
+    number += 1
+  end
+end
+
+result = forever.map do |value|
+  break if value >= 10
+  p value + 1
+end
+p result
+
+# why is result nil?
+
+
+
