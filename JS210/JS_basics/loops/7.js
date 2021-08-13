6fc82ecceeee61a5d06c67c9329d8bd47c33a16f
Ainaa Azman <ainaasakinah@gmail.com>
2021-08-13 21:16:57 +0900

    add question 7 practice

---
+++ JS210/JS_basics/loops/7.js
@@ -0,0 +1,9 @@
+// Fix the code so that it stops after the first iteration.
+
+for (let i = 0; ; i += 1) {
+  console.log("and on");
+  break;
+}
+
+// Assmuing that the question does not want us to add a conditional expression 
+// we can use  `break` to break out of the loop after the first iteration 
