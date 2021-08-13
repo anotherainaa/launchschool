07c3fd3a5cef8391cae74a0a74a614f12dfe41ad
Ainaa Azman <ainaasakinah@gmail.com>
2021-08-13 21:25:17 +0900

    add q10

--- JS210/JS_basics/loops/10.js
+++ JS210/JS_basics/loops/10.js
@@ -1,6 +1,17 @@
-let fish = ['Dory', 'Marlin', 'Gill', 'Nemo', 'Bruce'];
+// What's the difference between the two. 
 
-fish.forEach(name => {
-  console.log(name);
-  break if name === 'Nemo';
-});
+let counter1 = 0;
+
+while (counter1 > 0) {
+  console.log('Woooot!');
+  counter1 -= 1;
+}
+ 
+// The code below will run at least once, while do above would not.
+
+let counter2 = 0;
+
+do {
+  console.log('Woooot!');
+  counter2 -= 1;
+} while (counter2 > 0);
