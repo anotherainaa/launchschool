41be6a090d12553d94a9fee14ca036f79a2188e3
Ainaa Azman <ainaasakinah@gmail.com>
2021-08-13 21:01:10 +0900

    add question 3 practice

---
+++ JS210/JS_basics/loops/3.js
@@ -0,0 +1,18 @@
+// Write a loop that logs greeting three times.
+
+let greeting = 'Aloha!';
+let counter = 1
+
+// while (counter <= 3) {
+//   console.log(greeting);
+//   counter += 1;
+// }
+
+// Here we use a while loop. 
+// We must implement a counter to create a condition for this loop.
+
+// We can also use for loop. 
+
+for (let count = 1; count <= 3; count += 1) {
+  console.log(greeting);
+}
