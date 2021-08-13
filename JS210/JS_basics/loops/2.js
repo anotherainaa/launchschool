53d97c8d0380f2562c76903d3b99065ee0fe92c2
Ainaa Azman <ainaasakinah@gmail.com>
2021-08-13 20:54:32 +0900

    add practice code and noteson question 2

---
+++ JS210/JS_basics/loops/2.js
@@ -0,0 +1,14 @@
+// Modify the code to log numbers from 10 to 1 instead, and then lo `Launch!`
+
+for (let i = 10; i >= 1; i -= 1) {
+  console.log(i);
+}
+
+console.log('Launch!');
+
+// Here, we change the initial expression to initialize i = 10. 
+// The condition also needs to be changed such that it will continue running as long as i
+// is greater than 1 
+// Then, we change the increment expression to `i -= 1` so that the number going in a decresing order 
+// Finally we make sure we add `console.log('Launch!')` to meet the instructions.
+
