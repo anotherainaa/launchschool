03b3976674a7a51cd7bd3799a6ee38887fd9b34e
Ainaa Azman <ainaasakinah@gmail.com>
2021-08-13 21:03:00 +0900

    add question 4 practice

---
+++ JS210/JS_basics/loops/4.js
@@ -0,0 +1,6 @@
+// Write a for loop that iterates over all numbers from 1 to 100, 
+// and outputs the result of multiplying each element by 2.
+
+for (let n = 1; n <= 100; n += 1 ) {
+  console.log(n * 2);
+}
