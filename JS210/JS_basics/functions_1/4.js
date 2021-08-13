9606da3d40fe450054898338d3683bf04f9fbca0
Ainaa Azman <ainaasakinah@gmail.com>
2021-08-13 23:34:07 +0900

    add all exercises from function_1

---
+++ JS210/JS_basics/functions_1/4.js
@@ -0,0 +1,5 @@
+function squaredNumber(num) {
+  return Math.pow(num, 2);
+}
+
+console.log(squaredNumber(3)); // 9
---
+++ JS210/JS_basics/functions_1/7.js
@@ -0,0 +1,4 @@
+console.log("Captain Ruby".replace('Ruby', 'Javascript'));
+
+console.log('Captain Ruby'.substring(0, 8) + 'JavaScript');
+console.log(('Captain Ruby').split(' ')[0] + ' Javascript');
