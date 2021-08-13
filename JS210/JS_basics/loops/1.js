ec96ffd7be3ae0defb7482e1976309d55108d162
Ainaa Azman <ainaasakinah@gmail.com>
2021-08-13 20:47:06 +0900

    add js basics loops question 1

---
+++ JS210/JS_basics/loops/1.js
@@ -0,0 +1,14 @@
+// Add code to log the current value of i. 
+// What sequence of numbers do you expect to be logged? 
+
+
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i)
+}
+
+// I'm expected numbers 0, 2, 4, 6, 8, and 10 to be logger. 
+// JS's for loop expects an initializer, a condition and incrementor as coded up on lin5. 
+// In the initializer we can tell the `i` is being intialized with a starting value of 0. 
+// In the condition, the loop will run until `i` is equal to 10 (10 included.)
+// In the incrementor, we can tell that `i` will increment by 2 each time. 
+// Hence, the resulting output will be 0, 2, 4, 6, 8, and 10 respectievly. 
