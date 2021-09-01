937a9906372c18377bc9d34a0d76f2b25c9271d3
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-02 00:30:34 +0900

    commit unsaved changes

--- JS210/lesson3/index_of_substring.js
+++ JS210/lesson3/index_of_substring.js
@@ -1,25 +1,6 @@
 // implement includes
 
 function includesSubstr(firstString, secondString) {
-  let count = 0;
-  for (let letter of secondString) {
-    for (let char of firstString) {
-      if (letter !== char) {
-        count = 0;
-        continue;
-      } else if (letter === char) {
-        count += 1;
-      }
-
-      console.log(count);
-      if (count === secondString.length) {
-        return true;
-      } 
-    }
-  }
-
-  console.log(count);
-  return count === secondString.length;
 }
 
 console.log(includesSubstr('Blue Whale', 'Whale'));  
