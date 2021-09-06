c5a37846057ad06de897f53aa85175a4b39bf78d
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-07 00:05:23 +0900

    add first attempt index substring

--- JS210/lesson3/index_of_substring.js
+++ JS210/lesson3/index_of_substring.js
@@ -1,14 +1,49 @@
-// implement includes
+function indexOf(firstString, secondString) {
+  for (let i = 0; i < firstString.length; i++) {
+    let count = 0;
+    let index = i;
+    for (let j = 0; j < secondString.length; j++) {
+      if (firstString[index] === secondString[j]) {
+        count += 1;
 
-function includesSubstr(firstString, secondString) {
+        if (count === secondString.length) {
+          return i;
+        }
+
+        index++;
+      }
+      
+    }
+  }
+  return -1;
 }
 
-console.log(includesSubstr('Blue Whale', 'Whale'));  
+console.log(indexOf('Some strings', 's'));                      // 5
+console.log(indexOf('Blue Whale', 'Whale'));                    // 5
+console.log(indexOf('Blue Whale', 'Blute'));                    // -1
+console.log(indexOf('Blue Whale', 'leB'));                      // -1                    
 
-// function indexOf(firstString, secondString) {
-//   // statements
-// }
+function lastIndexOf(firstString, secondString) {
+  for (let i = firstString.length - 1; i >= 0; i--) {
+    let count = 0;
+    let index = i;
+    for (let j = secondString.length - 1; j >= 0; j--) {
+      if (firstString[index] === secondString[j]) {
+        count += 1;
+        
+        if (count === secondString.length) {
+          return index;
+        }
+        
+        index--;
+      }
+      
+    }
+  }
 
-// function lastIndexOf(firstString, secondString) {
-//   // statements
-// }
+  return -1;
+}
+
+console.log(lastIndexOf('Some strings', 's'));                  // 11
+console.log(lastIndexOf('Blue Whale, Killer Whale', 'Whale'));  // 19
+console.log(lastIndexOf('Blue Whale, Killer Whale', 'all'));    // -1
