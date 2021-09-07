fb0b64983f0ea95caf3d823509a43def4a4c578e
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-07 22:50:19 +0900

    fix question3

--- JS210/js210_small_problems/easy1/3.js
+++ JS210/js210_small_problems/easy1/3.js
@@ -2,9 +2,11 @@ let rlSync = require('readline-sync');
 
 const METER_IN_SQUARE_FEET = 10.7639;
 
-roomLengthInMeters = rlSync.question("Enter the length of the room in meters: ")
-rommWidthInMeters =  rlSync.question("Enter the width of the room in meters: ")
+let length = rlSync.question("Enter the length of the room in meters: ");
+length = parseInt(length, 10);
+let width =  rlSync.question("Enter the width of the room in meters: ");
+width = parseInt(width, 10);
 
-roomAreaInMeters = roomLengthInMeters * rommWidthInMeters;
+roomAreaInMeters = length * width;
 roomAreaInSquareFeet = roomAreaInMeters * METER_IN_SQUARE_FEET;
-console.log(`The area of the room is ${Math.round(roomAreaInMeters), 2} square meters (${Math.round(roomAreaInSquareFeet, 2)} square feet).`)
+console.log(`The area of the room is ${roomAreaInMeters.toFixed(2)} square meters (${roomAreaInSquareFeet.toFixed(2)} square feet).`)
