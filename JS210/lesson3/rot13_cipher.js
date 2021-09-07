aab856d9a5df409452e13eb72bfbdee79372ba63
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-07 18:20:42 +0900

    minor refactor on rot13

--- JS210/lesson3/rot13_cipher.js
+++ JS210/lesson3/rot13_cipher.js
@@ -1,13 +1,13 @@
 function rot13(string) {
-  const STRING_ROTATION = 13;
+  const ALPHABET_ROTATION = 13;
 
   let newString = ''
   for (let index = 0; index < string.length; index += 1) {
     let charCode = string.charCodeAt(index);
     if (string[index].toUpperCase() >= 'A' && string[index].toUpperCase() <= 'M') {
-      charCode += STRING_ROTATION;
+      charCode += ALPHABET_ROTATION;
     } else if (string[index].toUpperCase() >= 'N' && string[index].toUpperCase() <= 'Z') {
-      charCode -= STRING_ROTATION;
+      charCode -= ALPHABET_ROTATION;
     } 
 
     newString += String.fromCharCode(charCode);
