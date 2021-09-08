71d989683937ebbfc15f6198d5e04503d05e4481
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-08 18:33:53 +0900

    add first attempt at Fibonacci

--- JS210/js210_small_problems/easy2/4.js
+++ JS210/js210_small_problems/easy2/4.js
@@ -1,1 +1,28 @@
 /// fibonacci
+
+
+function findFibonacciIndexByLength(length) {
+  let first = 1n;
+  let second = 1n;
+  let index = 2n;
+
+  while (true) {
+    index += 1n;
+
+    sum = first + second;
+    first = second;
+    second = sum;
+    
+    if (String(sum).length == length) {
+      return index;
+    }
+  }
+}
+
+// console.log(findFibonacciIndexByLength(2n) === 7n);  
+// console.log(findFibonacciIndexByLength(3n) === 12n);
+console.log(findFibonacciIndexByLength(10n) === 45n);
+console.log(findFibonacciIndexByLength(16n) === 74n);
+console.log(findFibonacciIndexByLength(100n) === 476n);
+// console.log(findFibonacciIndexByLength(1000n) === 4782n);
+// console.log(findFibonacciIndexByLength(10000n) === 47847n);
