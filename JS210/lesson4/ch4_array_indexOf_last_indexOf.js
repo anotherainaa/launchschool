e1c90fa1b31eb94cd62dce6e6de996f37736f04e
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-08 19:53:51 +0900

    lesson4 ch 4

---
+++ JS210/lesson4/ch4_array_indexOf_last_indexOf.js
@@ -0,0 +1,23 @@
+// function indexOf(array, searchValue) {
+//   for (let index = 0; index < array.length; index += 1) {
+//     if (array[index] === searchValue) {
+//       return index;
+//     }
+//   }
+//   return -1;
+// }
+
+// console.log(indexOf([1, 2, 3, 3], 3));         // 2
+// console.log(indexOf([1, 2, 3], 4));            // -1
+
+const lastIndexOf = (array, searchValue) => {
+  for (let index = array.length - 1; index >= 0; index -= 1) {
+    if (array[index] === searchValue) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+console.log(lastIndexOf([1, 2, 3, 3], 3));     // 3
+console.log(lastIndexOf([1, 2, 3], 4));        // -1
