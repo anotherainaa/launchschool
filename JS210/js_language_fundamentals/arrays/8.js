7e33e43061e19174382d93d2d8db00bc7d4bfb8e
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-09 17:16:39 +0900

    add slice impelementation

--- JS210/js_language_fundamentals/arrays/8.js
+++ JS210/js_language_fundamentals/arrays/8.js
@@ -1,62 +1,28 @@
-// function slice(array, begin, end) {
-//   begin = begin > array.length ? array.length : begin;
-//   end = end > array.length ? array.length : end;
+function slice(array, begin, end) {
+  let length = array.length;
 
-//   const result = [];
-
-//   for (let i = begin; i < end; i++) {
-//     result.push(array[i])
-//   }
-//   return result;
-// }
-
-// console.log(slice([1, 2, 3], 1, 2));               // [2]
-// console.log(slice([1, 2, 3], 2, 0));               // []
-// console.log(slice([1, 2, 3], 5, 1));               // []
-// console.log(slice([1, 2, 3], 0, 5));               // [1, 2, 3]
-
-// const arr1 = [1, 2, 3];
-// console.log(slice(arr1, 1, 3));                     // [2, 3]
-// console.log(arr1);                                  // [1, 2, 3]
-
-function splice(array, start, deleteCount, element1, elementN) {
-  start = start > array.length ? array.length : start;
-
-  let startToEndArrayLength = array.length - start;
-  deleteCount = deleteCount > startToEndArrayLength ? startToEndArrayLength : deleteCount;
-  
-  const newArray = [];
-  
-  for (let i = start; i < start + deleteCount; i++) {
-    newArray.push(array[i]);
+  if (begin > length) {
+    begin = length;
   }
 
-  if (element1) {
-    array[start] = element1;
+  if (end > length) {
+    end = length;
+  }
+  
+  let newArray= [];
+
+  for (let i = begin; i < end; i++) {
+    newArray.push(array[i]);
   }
 
   return newArray;
 }
 
-// console.log(splice([1, 2, 3], 1, 2));              // [2, 3]
-// console.log(splice([1, 2, 3], 1, 3));              // [2, 3]
-// console.log(splice([1, 2, 3], 1, 0));              // []
-// console.log(splice([1, 2, 3], 0, 1));              // [1]
-// console.log(splice([1, 2, 3], 1, 0, 'a'));         // []
+console.log(slice([1, 2, 3], 1, 2));               // [2]
+console.log(slice([1, 2, 3], 2, 0));               // []
+console.log(slice([1, 2, 3], 5, 1));               // []
+console.log(slice([1, 2, 3], 0, 5));               // [1, 2, 3]
 
-const arr2 = [1, 2, 3];
-console.log(splice(arr2, 1, 1, 'two'));             // [2]
-console.log(arr2);                                  // [1, "two", 3]
-
-const arr3 = [1, 2, 3];
-console.log(splice(arr3, 1, 2, 'two', 'three'));    // [2, 3]
-console.log(arr3);                                  // [1, "two", "three"]
-
-const arr4 = [1, 2, 3];
-console.log(splice(arr4, 1, 0));                    // []
-console.log(splice(arr4, 1, 0, 'a'));               // []
-console.log(arr4);                                  // [1, "a", 2, 3]
-
-const arr5 = [1, 2, 3];
-console.log(splice(arr5, 0, 0, 'a'));               // []
-console.log(arr5);                                  // ["a", 1, 2, 3]
+const arr1 = [1, 2, 3];
+console.log(slice(arr1, 1, 3));                     // [2, 3]
+console.log(arr1);                                  // [1, 2, 3]
