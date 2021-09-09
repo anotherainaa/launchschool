da661a4c94f7b312a13e58e6e68beecc4d4c8e5b
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-09 17:09:54 +0900

    add revision question 7

--- JS210/js_language_fundamentals/arrays/7.js
+++ JS210/js_language_fundamentals/arrays/7.js
@@ -79,3 +79,22 @@ function unshift(array, ...args) {
 
   return array.length;
 }
+
+// LS solution
+
+function shift(array) {
+  let result;
+
+  if (array.length != 0) {
+    result = array.splice(0, 1).pop();
+  }
+
+  return result;
+}
+
+function unshift(array, ...args) {
+  for (let i = 0; i < args.length; i += 1) {
+    array.splice(i, 0, args[i]);
+  }
+  return array.length;
+}
