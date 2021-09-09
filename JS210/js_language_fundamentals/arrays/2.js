857016b6f4a8935c0062dda78aa3f399979f4ab2
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-09 16:10:57 +0900

    experimenting with slice

--- JS210/js_language_fundamentals/arrays/2.js
+++ JS210/js_language_fundamentals/arrays/2.js
@@ -5,10 +5,16 @@ for (let i = 0; i < myArray.length; i += 1) {
   myOtherArray.push(myArray[i]);
 }
 
+let anotherArray = myArray.slice();
+
 myArray.pop();
+console.log(myArray);
 console.log(myOtherArray);
+console.log(anotherArray);
 
 myArray = [1, 2];
 
 console.log(myOtherArray);
+
+
 
