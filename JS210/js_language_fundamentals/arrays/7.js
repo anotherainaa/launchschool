02786536db492c9a4be809777dd48602ff20c884
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-09 17:06:36 +0900

    second attempt at question 7

--- JS210/js_language_fundamentals/arrays/7.js
+++ JS210/js_language_fundamentals/arrays/7.js
@@ -1,46 +1,42 @@
-function shift(arr) {
-  if (arr.length === 0) { return; };
+// function shift(arr) {
+//   if (arr.length === 0) { return; };
 
-  let firstElem = arr[0];
-  let length = arr.length - 1;
+//   let firstElem = arr[0];
+//   let length = arr.length - 1;
 
 
-  for (let index = 1; index <= length; index++) {
-    arr[index - 1] = arr[index];
+//   for (let index = 1; index <= length; index++) {
+//     arr[index - 1] = arr[index];
 
-    if (index === length) {
-      arr.length = length;
-    }
-  }
+//     if (index === length) {
+//       arr.length = length;
+//     }
+//   }
 
-  return firstElem;
-}
+//   return firstElem;
+// }
 
 
 // console.log(shift([1, 2, 3]));                // 1
 // console.log(shift([]));                       // undefined
 // console.log(shift([[1, 2, 3], 4, 5]));        // [1, 2, 3]
 
-// const testArray = [1, 2, 3];
-// console.log(shift(testArray));                // 1
-// console.log(testArray);                       // [2, 3]
+// function unshift(arr, ...args) {
+//   let indexLastElem = arr.length - 1;
+//   let numberOfElements = args.length;
+//   arr.length += numberOfElements;
+//   let newArrLength = arr.length;
 
-function unshift(arr, ...args) {
-  let indexLastElem = arr.length - 1;
-  let numberOfElements = args.length;
-  arr.length += numberOfElements;
-  let newArrLength = arr.length;
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     arr[i] = arr[indexLastElem];
+//     indexLastElem -= 1;
+//   }
 
-  for (let i = arr.length - 1; i > 0; i--) {
-    arr[i] = arr[indexLastElem];
-    indexLastElem -= 1;
-  }
-
-  for (let i = 0; i < args.length; i++) {
-    arr[i] = args[i];
-  }
-  return newArrLength;
-}
+//   for (let i = 0; i < args.length; i++) {
+//     arr[i] = args[i];
+//   }
+//   return newArrLength;
+// }
 
 console.log(unshift([1, 2, 3], 5, 6));        // 5
 console.log(unshift([1, 2, 3]));              // 3
@@ -51,3 +47,35 @@ console.log(shift(testArray));                // 1
 console.log(testArray);                       // [2, 3]
 console.log(unshift(testArray, 5));           // 3
 console.log(testArray);                       // [5, 2, 3]
+
+
+function shift(array) {
+   if (array.length === 0) {
+     return;
+   }
+
+  let shiftedElement = array[0]
+  const newLength = array.length - 1;
+
+  for (let i = 0; i < newLength; i += 1) {
+    array[i] = array[i + 1]
+  }
+
+  array.length = newLength;
+  return shiftedElement;
+}
+
+function unshift(array, ...args) {
+  const newLength = array.length + args.length;
+  const len = args.length;
+
+  for (let i = newLength - 1; i >= args.length - 1; i --) {
+    array[i] = array[i - len];
+  }
+
+  for (let i = 0; i < args.length; i++) {
+    array[i] = args[i];
+  }
+
+  return array.length;
+}
