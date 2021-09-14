bc28b2574864a9dff43282ac7c6ea7ae67f8155e
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-15 02:11:38 +0900

    first attempt object q7

---
+++ JS210/js_language_fundamentals/objects/7.js
@@ -0,0 +1,12 @@
+// fill in the blanks so it works
+
+function calculateBonus() {
+  return arguments[1] ? arguments[0] / 2 : 0;
+}
+
+console.log(calculateBonus(2800, true));               // 1400
+console.log(calculateBonus(1000, false));              // 0
+console.log(calculateBonus(50000, true));              // 25000
+
+// Because arguments are available locally within the function. (even though there is no parameter explicitly defined. )
+// This is because all functions have arguments function that is available locally.
