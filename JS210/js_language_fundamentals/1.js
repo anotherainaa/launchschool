7cfad8e4b4514d6d010f2fed4edd983e143534b5
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-14 17:35:30 +0900

    add object q1 and q2 first attempts

--- JS210/js_language_fundamentals/1.js
+++ JS210/js_language_fundamentals/1.js
@@ -13,4 +13,5 @@ myObject.a;
 
 
 // Line 11 is the bug because where we are trying to access the property `a` using bracket notation. 
-// In this case this would raise a referenceerror. 
+// In this case this would raise a referenceerror because JS sees a as a variable and not a string value. 
+// We must use a srting value to reference a property name using bracket notation. 
--- JS210/js_language_fundamentals/2.js
+++ JS210/js_language_fundamentals/2.js
@@ -10,3 +10,7 @@ const person = {
 };
 
 console.log(`${person.firstName} ${person.lastName}`);
+
+// My guess
+// Code on line 12 would log the function instead of the return value of the function 'Victor' and 'Reyes'. 
+// This is because we are not calling the function on line 12. We know this due to the lack of parenthese on the method calls. 
