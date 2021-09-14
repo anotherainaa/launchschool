15e3dcd27003381011dd70dbc9a1cdbfdafbc87a
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-14 17:34:58 +0900

    add object q2

---
+++ JS210/js_language_fundamentals/2.js
@@ -0,0 +1,12 @@
+// What is the result when trying out the code on the last line? 
+
+const person = {
+  firstName() {
+    return 'Victor';
+  },
+  lastName() {
+    return 'Reyes';
+  },
+};
+
+console.log(`${person.firstName} ${person.lastName}`);
