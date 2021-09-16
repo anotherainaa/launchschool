0cbc822eb6f20022b550138dd6e88d37c0770712
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-16 22:02:11 +0900

    add medium1 question1

---
+++ JS210/js_language_fundamentals/medium_2/1.js
@@ -0,0 +1,34 @@
+function processOrder(price, quantity, discount, serviceCharge, tax) {
+  if (!quantity) {
+    quantity = 1;
+  }
+
+  if (!discount) {
+    discount = 0;
+  }
+
+  if (!serviceCharge) {
+    serviceCharge = 0.1;
+  }
+
+  if (!tax) {
+    tax = 0.15;
+  }
+
+  return (price * quantity) * (1 - discount) * (1 + serviceCharge) * (1 + tax);
+}
+
+processOrder(100);      // 126.5
+
+// A variation. What is the limitation on this variation and how does it affect the result?
+// The fact that 0 is a falsy value?
+// You can never use the value of 0 on quantity, serviceCharge and tax.
+
+function processOrder(price, quantity, discount, serviceCharge, tax) {
+  quantity = quantity || 1;
+  discount = discount || 0;
+  serviceCharge = serviceCharge || 0.1;
+  tax = tax || 0.15;
+
+  return (price * quantity) * (1 - discount) * (1 + serviceCharge) * (1 + tax);
+}
