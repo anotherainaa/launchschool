481241b73191b553ef54a754ef12cf88566e29ee
Ainaa Azman <ainaasakinah@gmail.com>
2021-11-02 15:25:19 +0900

    first attempt at word to digit

--- LS215/medium_problems1/word_to_digit.js
+++ LS215/medium_problems1/word_to_digit.js
@@ -13,27 +13,51 @@
 //   }).join(' ');
 // }
 
+// const NUM_WORDS = {
+//   zero:  0,
+//   one:   1,
+//   two:   2,
+//   three: 3,
+//   four:  4,
+//   five:  5,
+//   six:   6,
+//   seven: 7,
+//   eight: 8,
+//   nine:  9,
+// };
+
+// function wordToDigit(sentence) {
+//   Object.keys(NUM_WORDS).forEach(word => {
+//     let regex = new RegExp(`\\bword\\b`, 'g'); 
+//     sentence = sentence.replace(regex, NUM_WORDS[word]);
+//   })
+
+//   return sentence;
+// }
+
+// Without looping
+
 const NUM_WORDS = {
-  zero:  0,
-  one:   1,
-  two:   2,
-  three: 3,
-  four:  4,
-  five:  5,
-  six:   6,
-  seven: 7,
-  eight: 8,
-  nine:  9,
-};
-
-function wordToDigit(sentence) {
-  Object.keys(NUM_WORDS).forEach(word => {
-    let regex = new RegExp(word, 'g'); 
-    sentence = sentence.replace(regex, NUM_WORDS[word]);
-  })
-
-  return sentence;
+  zero:   0, 
+  one:    1,
+  two:    2,
+  three:  3,
+  four:   4,
+  five:   5,
+  six:    6,
+  seven:  7,
+  eight:  8,
+  nine:   9,
 }
 
+const REGEX = new RegExp('\\b(' + Object.keys(NUM_WORDS).join('|') + ')\\b',
+                         'gi');
+
+function wordToDigit(string) {
+  return string.replace(REGEX, word => NUM_WORDS[word]);
+}
+
+
 console.log(wordToDigit('Please call me at five five five one two three four. Thanks.'));
+console.log(wordToDigit('The weight is done.')); 
 // "Please call me at 5 5 5 1 2 3 4. Thanks."
