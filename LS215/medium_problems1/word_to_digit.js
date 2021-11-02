142981409519fcb2a5f75dde96d293ca4cd8451d
Ainaa Azman <ainaasakinah@gmail.com>
2021-11-02 15:34:21 +0900

    experiment with word to digit regex

--- LS215/medium_problems1/word_to_digit.js
+++ LS215/medium_problems1/word_to_digit.js
@@ -35,26 +35,28 @@
 //   return sentence;
 // }
 
-// Without looping
+// Wthout looping
 
-const NUM_WORDS = {
-  zero:   0, 
-  one:    1,
-  two:    2,
-  three:  3,
-  four:   4,
-  five:   5,
-  six:    6,
-  seven:  7,
-  eight:  8,
-  nine:   9,
-}
+// const NUM_WORDS = {
+//   zero:   0, 
+//   one:    1,
+//   two:    2,
+//   three:  3,
+//   four:   4,
+//   five:   5,
+//   six:    6,
+//   seven:  7,
+//   eight:  8,
+//   nine:   9,
+// }
 
-const REGEX = new RegExp('\\b(' + Object.keys(NUM_WORDS).join('|') + ')\\b',
-                         'gi');
+const NUM_WORDS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+
+const REGEX = new RegExp('\\b(' + NUM_WORDS.join('|') + ')\\b', 'gi');
 
 function wordToDigit(string) {
-  return string.replace(REGEX, word => NUM_WORDS[word]);
+  return string.replace(REGEX, word => NUM_WORDS.indexOf(word));
 }
 
 
