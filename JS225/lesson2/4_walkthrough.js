dd2c5b5d0bfbba09b5370aef6454b1fd5db4f03c
Ainaa Azman <ainaasakinah@gmail.com>
2021-12-22 16:28:10 +0900

    more walkthrough

--- JS225/lesson2/4_walkthrough.js
+++ JS225/lesson2/4_walkthrough.js
@@ -75,10 +75,77 @@ let people2 = {
   },
 
   add: function(person) {
+    if (this.isInvalidPerson(person)) {
+      return;
+    }
+
     this.collection.push(person);
+  },
+
+  get: function(person) {
+    if (this.isInvalidPerson(person)) {
+      return;
+    }
+
+    return this.collection[this.getIndex(person)];
+  },
+
+  update: function(person) {
+    if (this.isInvalidPerson(person)) {
+      return;
+    }
+
+    let existingPersonId = this.getIndex(person);
+    if (existingPersonId === -1) {
+      this.add(person);
+    } else {
+      this.collection[existingPersonId] = person;
+    }
+  },
+
+  getIndex: function(person) {
+    let index = -1;
+    this.collection.forEach(function(comparator, i) {
+      if (comparator.firstName === person.firstName && comparator.lastName === person.lastName) {
+        index = i;
+      }
+    });
+    
+    return index;
+  },
+
+  remove: function(person) {
+    let index;
+    if (this.isInvalidPerson(person)) {
+      return;
+    }
+
+    index = this.getIndex(person);
+    if (index === -1) {
+      return;
+    }
+
+    this.collection.splice(index, 1);
+  },
+
+  isInvalidPerson: function(person) {
+    return typeof person.firstName !== 'string' || typeof person.lastName !== 'string';
   },
 }
 
 
 
 people2.rollcall();
+
+people2.remove({ firstName: 'Ainaa', lastName: 'Doe' })
+
+console.log(people2);
+
+
+people2.update({ firstName: 'Amir', lastName: 'Doe'});
+
+console.log(people2.get({firstName: 'Amir', lastName: 'Doe'}));
+console.log(people2);
+
+// Further exploration
+// Add an index or ID that increments each time we add a new person.
