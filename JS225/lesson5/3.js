39470cf51f7c97893a496a3f3a6511f173b601c1
Ainaa Azman <ainaasakinah@gmail.com>
2022-01-08 22:51:04 +0900

    practice problem factory function

---
+++ JS225/lesson5/3.js
@@ -0,0 +1,15 @@
+// Problem 1
+
+// - can't tell whether the object is created by any function
+// - each object owns a copy of the same methods
+
+// Problem 2 
+
+function makeObj() {
+  return obj {
+    propA: 10;
+    propB: 20;
+  };
+}
+
+// Problem 3
