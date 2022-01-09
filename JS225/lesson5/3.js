c34a9ea7f86c7cbf8d166151d54787d6ffec9c71
Ainaa Azman <ainaasakinah@gmail.com>
2022-01-09 22:58:57 +0900

    add last practice problem for ff review

--- JS225/lesson5/3.js
+++ JS225/lesson5/3.js
@@ -68,3 +68,104 @@ invoices.push(createInvoice({
 }));
 
 console.log(invoiceTotal(invoices));
+
+// Problem 4
+
+function createPayment(services = {}) {
+  return {
+    phone: services.phone || 0,
+    internet: services.internet || 0,
+    amount: services.amount,
+    total() {
+      return this.amount || (this.phone + this.internet);
+    }
+  }
+}
+
+function paymentTotal(payments) {
+  let total = 0;
+  let i;
+
+  for (let i = 0; i < payments.length; i += 1) {
+    total += payments[i].total();
+  }
+
+  return total;
+}
+
+let payments = [];
+payments.push(createPayment());
+payments.push(createPayment({
+  internet: 6500,
+}));
+
+payments.push(createPayment({
+  phone: 2000,
+}));
+
+payments.push(createPayment({
+  phone: 1000,
+  internet: 4500,
+}));
+
+payments.push(createPayment({
+  amount: 10000,
+}));
+
+console.log(paymentTotal(payments)); 
+
+
+// Problem 5
+
+function createInvoice(services = {}) {
+  return {
+    phone: services.phone || 3000,
+    internet: services.internet || 5500,
+    paymentsList: [],
+    
+    total() {
+      return this.phone + this.internet;
+    },
+
+    addPayment(payment) {
+      this.paymentsList.push(payment);
+    },
+
+    addPayments(payments) {
+      payments.forEach(payment => this.addPayment(payment));
+    },
+
+    paymentTotal() {
+      return this.paymentsList.reduce((total, payment) => total + payment.total(), 0);
+    },
+
+    amountDue() {
+      return this.total() - this.paymentTotal();
+    } 
+  };
+}
+
+
+let invoice = createInvoice({
+  phone: 1200,
+  internet: 4000,
+});
+
+let payment1 = createPayment({
+  amount: 2000,
+});
+
+let payment2 = createPayment({
+  phone: 1000,
+  internet: 1200,
+});
+
+let payment3 = createPayment({
+  phone: 1000,
+});
+
+invoice.addPayment(payment1);
+invoice.addPayments([payment2, payment3]);
+console.log(invoice.amountDue());       // this should return 0
+
+
