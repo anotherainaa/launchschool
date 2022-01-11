e91fa9da6f0da137a9fea9120bc5437a0c3749de
Ainaa Azman <ainaasakinah@gmail.com>
2022-01-11 23:16:40 +0900

    revision on PFA

---
+++ JS225/lesson4/review/12_pp_pfa.js
@@ -0,0 +1,77 @@
+// # Practice Problems: Partial Function Application
+
+// Problem 1
+
+// function subtract(a, b) {
+//   return a - b;
+// }
+
+// function sub5(a) {
+//   return subtract(a, 5);
+// }
+
+// console.log(sub5(10)); // 5
+// console.log(sub5(20)); // 15
+
+// Problem 2
+
+// function subtract(a, b) {
+//   return a - b;
+// }
+
+// function makeSubN(n) {
+//   return function(a) {
+//     return subtract(a, n);
+//   }
+// }
+
+// let sub5 = makeSubN(5);
+// console.log(sub5(10)); // 5
+
+// Problem 3
+
+// function makePartialFunc(func, b) {
+//   return function(a) {
+//     return func(a, b);
+//   }
+// }
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// let multiplyBy5 = makePartialFunc(multiply, 5);
+
+// console.log(multiplyBy5(100)); // 500
+
+// Problem 4
+
+// Answer - closures. multiplyBy5 retains access to the variables that are defined in the function's outer scope
+
+// Problem 5
+
+let subjects = {
+  English: ['Bob', 'Tyrone', 'Lizzy'],
+  Math: ['Fatima', 'Gary', 'Susan'],
+  Biology: ['Jack', 'Sarah', 'Tanya'],
+};
+
+function rollCall(subject, students) {
+  console.log(subject + ':');
+  students.forEach(function(student) {
+    console.log(student);
+  });
+}
+
+function makeMathRollCall() {
+  return function(students) {
+    return rollCall('Math', students);
+  }
+}
+
+let mathRollCall = makeMathRollCall();
+mathRollCall(subjects['Math']);
+// => Math:
+// => Fatima
+// => Gary
+// => Susan
