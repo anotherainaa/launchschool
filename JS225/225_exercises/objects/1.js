fb49c63a6bab292e9b593faecf40afcd5553c955
Ainaa Azman <ainaasakinah@gmail.com>
2022-01-12 16:50:13 +0900

    exercises 1

---
+++ JS225/225_exercises/objects/1.js
@@ -0,0 +1,34 @@
+function createGreeter(name) {
+  return {
+    name,
+    morning: 'Good Morning',
+    afternoon: 'Good Afternoon',
+    evening: 'Good Evening',
+    greet(timeOfDay) {
+      let msg = '';
+      switch (timeOfDay) {
+        case 'morning':
+          msg += `${this.morning} ${name}`;
+          break;
+        case 'afternoon':
+          msg += `${this.afternoon} ${name}`;
+          break;
+        case 'evening':
+          msg += `${this.evening} ${name}`;
+          break;
+      }
+
+      console.log(msg);
+    },
+  };
+}
+
+const helloVictor = createGreeter('Victor');
+helloVictor.greet('evening');
+
+// Why isn't it producing the expected results?
+// - raises an error, becases we didn;t use this
+// - Have to use this to define the execution context
+
+// The alternative solution uses this.name why does both work
+// we have access to the parameter name as well as the property this.name set to name
