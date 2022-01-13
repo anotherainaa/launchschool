249cefc22a87cb54dd31fabb28abf202d39f6699
Ainaa Azman <ainaasakinah@gmail.com>
2022-01-13 14:41:16 +0900

    save progress

--- JS225/225_exercises/function_context_scope_closures/8.js
+++ JS225/225_exercises/function_context_scope_closures/8.js
@@ -1,3 +1,31 @@
-function newStack() {
-  
-}
+/* PEDAC
+
+Problem
+
+- a stack is like an array
+- a stack has a rule
+  - add new elements in any order but remove only the last inserted element
+
+- create a function new Stack
+  - when callen, returns a new stack object 
+
+Input : nothing? 
+Output: a stack object with 3 methods
+  - push
+  - pop
+  - printStack
+
+push
+input: value
+appends it to the stack
+
+pop
+input: none
+removes and returns the last element from the stack
+
+printStack
+input: none
+logs each remaining element of the stack
+
+
+*/
