05f9f4f95130483efe382f8360368045fb251cc3
Ainaa Azman <ainaasakinah@gmail.com>
2022-01-13 15:23:53 +0900

    add myStack practice problem

--- JS225/225_exercises/function_context_scope_closures/8.js
+++ JS225/225_exercises/function_context_scope_closures/8.js
@@ -25,7 +25,43 @@ removes and returns the last element from the stack
 
 printStack
 input: none
-logs each remaining element of the stack
+logs each remaining element of the stack from first added to last added
+
+Data Structure 
+- use an array to implement the stack 
+  - the array must be private - it cannot be accessed outside of the methods!
+
+Test cases
+
+[]
+newStack.push()
 
 
 */
+
+function newStack() {
+  const stack = [];
+  return {
+    push(element) {
+      stack.push(element);
+    },
+    pop() {
+      return stack.pop();
+    },
+    printStack() {
+      stack.forEach(elem => console.log(elem));
+    },
+  };
+}
+
+const myStack = newStack();
+
+myStack.push(1);
+myStack.push(44);
+myStack.push(99);
+myStack.push(102);
+console.log(myStack.pop()); // 102
+myStack.printStack(); // logs 1, then 44 then 99
+
+console.log(myStack);
+
