fe9ac90cb24a048e68c76a57db87370bfc1f5911
Ainaa Azman <ainaasakinah@gmail.com>
2022-01-15 21:37:06 +0900

    save progress

---
+++ JS225/225_exercises/object_creation_patterns/5.js
@@ -0,0 +1,19 @@
+// mini inventory management system 
+
+// SKU Code
+
+/*
+
+PEDAC
+
+
+*/
+
+
+// this is a fuction declaration - it's a statement
+
+// function expression
+
+(function random() {
+  console.log('Hello world')
+})();
