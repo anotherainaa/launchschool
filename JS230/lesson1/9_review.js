72cd7beaa5940e8144a5a3e039b8ae9fe1eb8d02
Ainaa Azman <ainaasakinah@gmail.com>
2022-01-31 23:02:25 +0900

    add revision to traversing and accessing attributes

---
+++ JS230/lesson1/9_review.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let html = document.childNodes[0];
+  let body = document.lastChild;
+  let ps = body.getElementsByTagName("p")
+
+  let psArr = [].slice.call(ps);
+
+  psArr.forEach(element => {
+    element.style.color = 'green';
+  });
+
+  function walk(node, callback) {
+    callback(node);
+
+    for (let i = 0; node.childNodes.length; i += 1) {
+      walk(node.childNodes[i], callback);
+    }
+  }
+})
