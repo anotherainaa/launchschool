08d8de9ac03c8131f91b11d0b9dcf038ce4b37b3
Ainaa Azman <ainaasakinah@gmail.com>
2022-02-01 12:48:36 +0900

    add solution to gremlin assignment

--- JS230/lesson1/dom_shuffling_completed/my_code.js
+++ JS230/lesson1/dom_shuffling_completed/my_code.js
@@ -6,15 +6,24 @@ let figures = document.getElementsByTagName('figure');
 let figure1 = figures[0];
 let figure2 = figures[1];
 
-let chin = figure1.replaceChild(images[1], figures[0].childNodes[0]);
+figure1.replaceChild(images[1], figure1.childNodes[0]);
+figure2.insertBefore(images[1], figure2.firstChild);
+
+let h1 = document.body.childNodes[1].childNodes[1];
+let main = document.body.childNodes[1];
+
+main.removeChild(h1);
+
+let header = document.body.lastChild.previousElementSibling.previousElementSibling;
+
+header.insertBefore(h1, header.childNodes[0])
+document.body.removeChild(header);
+document.body.insertBefore(header, main);
+
+main.firstElementChild.removeChild(figure1);
+main.firstElementChild.removeChild(figure2);
 
 
-// let removed = figures[0].removeChild(figures[0].childNodes[1]);
+main.firstElementChild.firstElementChild.appendChild(figure1);
+main.firstElementChild.firstElementChild.appendChild(figure2);
 
-// console.log(images);
-// console.log(figure1);
-// console.log(figure2);
-figures[1].insertBefore(images[1], figures[1].firstChild);
-
-
-// figures.replaceChild(, figure1)
