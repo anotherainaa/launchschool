e5c6e777a3aa2ff4d6e58de32a94613a50cc4f7e
Ainaa Azman <ainaasakinah@gmail.com>
2022-02-02 18:10:18 +0900

    add practice problem 10

---
+++ JS230/lesson3/10.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let store = document.getElementById('store');
+  
+  let request = new XMLHttpRequest();
+  request.open('GET', 'https://ls-230-web-store-demo.herokuapp.com/products');
+
+  request.addEventListener('load', event => store.innerHTML = request.response);
+  request.send();
+  
+  store.addEventListener('click', event =>  {
+    let target = event.target;
+    if (target.tagName !== 'A') {
+      return;
+    }
+    
+    event.preventDefault();
+  
+    let request = new XMLHttpRequest();
+
+    request.open('GET', `https://ls-230-web-store-demo.herokuapp.com${target.getAttribute('href')}`);
+
+    request.addEventListener('load', event => store.innerHTML = request.response);
+    request.send();
+
+
+  });
+
+  store.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let form = event.target;
+    let request = new XMLHttpRequest();
+    let data = new FormData(form);
+
+    request.open('POST', 'https://ls-230-web-store-demo.herokuapp.com/products');
+    request.setRequestHeader('Authorization', 'token AUTH_TOKEN');
+
+    request.addEventListener('load', event => store.innerHTML = request.response);
+
+    request.send(data);
+  });
+
+
+
+});
