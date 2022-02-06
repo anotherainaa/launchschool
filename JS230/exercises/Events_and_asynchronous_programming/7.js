eecbf406577bf482ec6f39250189851973820992
Ainaa Azman <ainaasakinah@gmail.com>
2022-02-06 20:55:01 +0900

    add solution to article highlighter

--- JS230/exercises/Events_and_asynchronous_programming/7.js
+++ JS230/exercises/Events_and_asynchronous_programming/7.js
@@ -1,1 +1,52 @@
 // we need to scroll
+document.addEventListener('DOMContentLoaded', () => {
+  function removehighlight() {
+    let currentHighlight = document.querySelector('.highlight');
+      
+    if (currentHighlight) {
+      currentHighlight.classList.remove('highlight');
+    }
+  }
+
+  function addHighlight(element) {
+    element.classList.add('highlight');
+  }
+
+
+  let links = document.querySelector('ul');
+  links.addEventListener('click', event => {
+    event.stopPropagation();
+
+    if (event.target.tagName === 'A') {
+      removehighlight();
+      let articleId = event.target.textContent.toLowerCase().split(' ').join('-');
+      let currentArticle = document.getElementById(articleId);
+      addHighlight(currentArticle); 
+    }
+  });
+
+  let articles = [].slice.call(document.querySelectorAll('article'));
+
+  articles.forEach(article => {
+    article.addEventListener('click', event => {
+      event.stopPropagation();
+
+      removehighlight();
+      addHighlight(article);
+    })    
+  });
+
+  document.addEventListener('click', event => {
+    articles.forEach(article => {
+      if (!article.contains(event.target)) {
+        removehighlight();
+        let main = document.querySelector('main');
+        addHighlight(main);
+      }
+    })
+  })
+
+})
+
+
+// clear highlight from other articles 
