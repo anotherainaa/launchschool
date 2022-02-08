bfae0e25c4a2ad11530f5f8cb011464a36c6faef
Ainaa Azman <ainaasakinah@gmail.com>
2022-02-08 15:22:44 +0900

    add notes on exercise 3

---
+++ JS230/exercises/making_HTTP_Requests/2.js
@@ -0,0 +1,25 @@
+/*
+
+
+Input: API - ajax from API? 
+Output: 
+- Alert as "key:value" pairs
+- staff id as key, and the count of schedules as value
+- format 'staff {id}' and {scheduleCount}
+
+Rules
+- if there are more than one schedule
+  - tally the count of schedule for each staff
+  - alert the user
+- if there are no schedules
+  - alert the user there are currently no schedules available for booking
+
+Edge cases
+- The server will slow down when there are more than 7 schedules
+- Note that it DOESN'T ALWAYS HAPPEN
+How to handle it? 
+  - if more than 5 seconds passed (5000ms), cancel the retrieval(avo) and inform the user to try again.
+
+
+
+*/
