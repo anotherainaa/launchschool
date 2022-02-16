1209b3ffe537fc14826108048d7e3dd13baaad76
Ainaa Azman <ainaasakinah@gmail.com>
2022-02-17 00:17:49 +0900

    add notes

---
+++ JS230/review_lesson2/play.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let html = document.querySelector('html');
+  let body = document.querySelector('body');
+  let button = document.getElementById('demo');
+  let messages = document.getElementById('messages');
+  let phases = {
+    1: 'capture',
+    2: 'target',
+    3: 'bubble',
+  }
+
+  function logClickedMessage(event) {
+    let currentTarget = event.currentTarget.tagName;
+    let phaseName = phases[event.eventPhase];
+    let message = 'Click event triggered during ' + phaseName + ' phase at ' + currentTarget;
+    message += ' And this event was previously handled at ' + event.previouslyHandledTagName;
+    messages.innerHTML += message + '<br/>';
+    event.previouslyHandledTagName = currentTarget;
+  }
+
+  html.addEventListener('click', logClickedMessage);
+  body.addEventListener('click', logClickedMessage);
+  button.addEventListener('click',logClickedMessage);
+})
