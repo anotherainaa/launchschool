fec836afedc6749f236d52d95e5b869d08708ccb
Ainaa Azman <ainaasakinah@gmail.com>
2022-02-18 02:03:51 +0900

    save progress

--- JS230/lesson5/company_site/team.js
+++ JS230/lesson5/company_site/team.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('hello');
+  document.addEventListener('click', event => {
+    let images = document.querySelectorAll('img');
+
+    images.forEach(image => {
+      if (event.target.contains(image)) {
+        event.preventDefault();
+        console.log('team member clicked');
+
+        // // Create a modal
+        // let link = event.target.closest('a');
+        // let modal = document.querySelector('#modal');
+        // let modalLayer = document.querySelector('#modal-layer');
+        // modal.style.display = 'block';
+        // modalLayer.classList.replace('hide', 'show');
+        // modal.classList.replace('hide', 'show');
+      }
+    })
+  })
 
 });
